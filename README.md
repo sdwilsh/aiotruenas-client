41e2f846c511410d63083d0e0c635a9a68d824fa
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2020-08-08 12:30:21 -0700

    Initial import from https://github.com/sdwilsh/hass-freenas

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python Module for FreeNAS Websocket API
+
+## Development
+
+```
+python3.8 -m venv .venv
+source .venv/bin/activate
+
+# Install Requirements
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+"""pyfreenas setup script."""
+from setuptools import setup
+
+version = "0.1.0"
+
+github_username = "sdwilsh"
+github_repository = "py-freenas"
+
+github_path = f"{github_username}/{github_repository}"
+github_url = f"https://github.com/{github_path}"
+
+download_url = f"{github_url}/archive/{version}.tar.gz"
+project_urls = {"Bug Reports": f"{github_url}/issues"}
+
+setup(
+    name="pyfreenas",
+    version=version,
+    author="Shawn Wilsher",
+    author_email="me@shawnwilsher.com",
+    packages=["pyfreenas"],
+    install_requires=["websockets==8.1", "meteor-ejson==1.1.0"],
+    url=github_url,
+    download_url=download_url,
+    project_urls=project_urls,
+    license="MIT",
+    description="A Python module for the FreeNAS websocket API.",
+    platforms="Cross Platform",
+)
