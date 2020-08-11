d244887199dd18baf742d542b28f1b1dec1ba2fa
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2020-08-10 21:26:19 -0700

    Add more details to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """pyfreenas setup script."""
-from setuptools import setup
+import setuptools
 
 version = "0.1.0"
 
@@ -12,18 +12,27 @@ github_url = f"https://github.com/{github_path}"
 download_url = f"{github_url}/archive/{version}.tar.gz"
 project_urls = {"Bug Reports": f"{github_url}/issues"}
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="pyfreenas",
     version=version,
     author="Shawn Wilsher",
     author_email="me@shawnwilsher.com",
-    packages=["pyfreenas"],
-    install_requires=["websockets==8.1", "meteor-ejson==1.1.0"],
+    description="A Python module for the FreeNAS websocket API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url=github_url,
+    packages=setuptools.find_packages(),
+    install_requires=["websockets==8.1", "meteor-ejson==1.1.0"],
     download_url=download_url,
     project_urls=project_urls,
-    license="MIT",
-    description="A Python module for the FreeNAS websocket API.",
-    platforms="Cross Platform",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+    ],
     python_requires=">3.8, <4",
 )
