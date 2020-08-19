db605434cfb658adab2d38139f12a04e76b9167b
tobiasbp <tobiasbp@gmail.com>
2020-08-19 09:21:24 +0200

    Added pool states from 'zfs' man page

--- pyfreenas/pool.py
+++ pyfreenas/pool.py
@@ -7,19 +7,31 @@ TPoolStatus = TypeVar("TType", bound="PoolStatus")
 
 @unique
 class PoolStatus(Enum):
+    # States from "man zpool"
     DEGRADED = "DEGRADED"
+    FAULTED = "FAULTED"
     OFFLINE = "OFFLINE"
     ONLINE = "ONLINE"
+    REMOVED = "REMOVED"
+    UNAVAIL = "UNAVAIL"
+    # State unique to FreeNAS (AFAIK)
+    # Pool known by FreeNAS, but not by zfs (I think)
     UNKNOWN = "UNKNOWN"
 
     @classmethod
     def fromValue(cls, value: str) -> TPoolStatus:
         if value == cls.DEGRADED.value:
             return cls.DEGRADED
+        if value == cls.FAULTED.value:
+            return cls.FAULTED
         if value == cls.OFFLINE.value:
             return cls.OFFLINE
         if value == cls.ONLINE.value:
             return cls.ONLINE
+        if value == cls.REMOVED.value:
+            return cls.REMOVED
+        if value == cls.UNAVAIL.value:
+            return cls.UNAVAIL
         if value == cls.UNKNOWN.value:
             return cls.UNKNOWN
         raise Exception(f"Unexpected pool status '{value}'")
